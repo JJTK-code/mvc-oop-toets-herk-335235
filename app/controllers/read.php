5a27f085258afdea5b411bc6cc5d0286a8dfdb4f
JJTK-code <335235@student.mboutrecht.nl>
2023-03-31 10:41:56 +0200

    Forth commit

--- app/controllers/read.php
+++ app/controllers/read.php
@@ -1,28 +1,40 @@
 <?php
+include('config.php');
 
-
-require "config.php";
-
-$conn = new mysql($dbHost, $dbName, $dbUser, $dbPass);
-
-if ($conn->connect_error) {
-    die("Verbinding met databank mislukt: " . $conn->connect_error);
+$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=UTF8";
+//connect read.php to database php-pdo-crud-herkansing
+try {
+    $pdo = new PDO($dsn, $dbUser, $dbPass);
+    if ($pdo) {
+        
+    } else {
+        echo "Interne server-error";
+    }  
+} catch (PDOException $e) {
+    $e->getMessage();
 }
 
-$sql = "SELECT * FROM poundforpound";
+//Haal alle gegevens op uit de database
+$sql = "SELECT * FROM PoundForPound;";
+$statement = $pdo->prepare($sql);
+$statement->execute();
 
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        ?>
-        <tr>
-            <td><?$echo $row["Name"]</td>
-            <td><?$echo $row["Ranking"]</td>
-            <td><?$echo $row["Length"]</td>
-            <td><?$echo $row["Weight"]</td>
-            <td><?$echo $row["Age"]</td>
-            <td><?$echo $row["WinsByKnockout"]</td>
-        </tr>
-    }
+//Zet de opgehaalde gegevens in een array van object
+$result = $statement->fetchAll(PDO::FETCH_OBJ);
+//var_dump($result);
+//echo $result[5]->Voornaam
+
+$tableRows = "";
+
+foreach($result as $info) {
+    $tableRows .= "<tr>
+                        <td>$info->Name</td>
+                        <td>$info->Ranking</td>
+                        <td>$info->Length</td>
+                        <td>$info->Weight</td>
+                        <td>$info->Age</td>
+                        <td>$info->WinsByKnockout</td>
+                    </tr>";
 }
+?>
--- app/controllers/view.php
+++ app/controllers/view.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-<a href='tabel.php'><button></button></a>
+    <?php include('read.php'); ?>
+    <div class="tabel">
+        <table>
+            <tr>
+            <h1>Men's pund-for-pound top rank ufc</h1>
+                <th>Name</th>
+                <th>Ranking</th>
+                <th>Length</th>
+                <th>Weight</th>
+                <th>Age</th>
+                <th>WinsByKnockout</th>
+            </tr>
+            <?php echo $tableRows; ?>
+        </table>
+    </div>
+    
+
 </body>
 </html>
